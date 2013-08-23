a4ff45e12ec37452656d476df5127f66ef492072
Maxime Guennec <maxime.guennec@gmail.com>
2013-08-23 13:23:42 +0200

    externalisation of the login box

--- src/main/java/botleecher/client/BotLeecherGwt.java
+++ src/main/java/botleecher/client/BotLeecherGwt.java
@@ -1,21 +1,15 @@
 package botleecher.client;
 
 import botleecher.client.component.BotPanel;
+import botleecher.client.component.LoginBox;
 import botleecher.client.domain.SessionClient;
-import botleecher.server.security.SessionManager;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.event.dom.client.KeyCodes;
-import com.google.gwt.event.dom.client.KeyUpEvent;
-import com.google.gwt.event.dom.client.KeyUpHandler;
 import com.google.gwt.i18n.client.Messages;
 import com.google.gwt.user.client.Cookies;
 import com.google.gwt.user.client.rpc.AsyncCallback;
-import com.google.gwt.user.client.ui.*;
-import com.sencha.gxt.widget.core.client.button.TextButton;
-import com.sencha.gxt.widget.core.client.event.SelectEvent;
-
-import java.util.Date;
+import com.google.gwt.user.client.ui.DialogBox;
+import com.google.gwt.user.client.ui.RootPanel;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -35,6 +29,10 @@ public class BotLeecherGwt implements EntryPoint {
     private static DialogBox dialogBox;
     private final Messages messages = GWT.create(Messages.class);
     private static LoginServiceAsync loginService = LoginService.App.getInstance();
+
+    public static void setSession(SessionClient session) {
+        BotLeecherGwt.session = session;
+    }
 
     public static SessionClient getSession() {
         return session;
@@ -105,63 +103,7 @@ public class BotLeecherGwt implements EntryPoint {
     }
 
     private static void createDialogBox() {
-        dialogBox = new DialogBox();
-        dialogBox.setText("Login Verification");
-        final VerticalPanel verticalPanel = new VerticalPanel();
-        dialogBox.add(verticalPanel);
-        final Label error = new Label();
-        verticalPanel.add(error);
-        final TextBox loginBox = new TextBox();
-        final PasswordTextBox passwordBox = new PasswordTextBox();
-        final Grid grid = new Grid(2, 2);
-        grid.setWidget(0, 0, new Label("Login"));
-        grid.setWidget(1, 0, new Label("Password"));
-        grid.setWidget(0, 1, loginBox);
-        grid.setWidget(1, 1, passwordBox);
-        verticalPanel.add(grid);
-
-        final TextButton loginButton = new TextButton("Login");
-        loginButton.addSelectHandler(new SelectEvent.SelectHandler() {
-            @Override
-            public void onSelect(SelectEvent selectEvent) {
-                try {
-                    final String login = loginBox.getText();
-                    loginService.login(login, passwordBox.getText(), new AsyncCallback<String>() {
-
-                        @Override
-                        public void onFailure(Throwable caught) {
-                            error.setText("Error during login.");
-                        }
-
-                        @Override
-                        public void onSuccess(String result) {
-                            if (result == null) {
-                                error.setText("Wrong Login/Password.");
-                            } else {
-                                session = new SessionClient(login, result);
-                                Date expires = new Date(System.currentTimeMillis() + SessionManager.SESSION_DURATION);
-                                Cookies.setCookie(SID_COOKIE_NAME, result, expires, null, "/", false);
-                                Cookies.setCookie(USER_COOKIE_NAME, login, expires, null, "/", false);
-                                showPanel();
-                            }
-                        }
-                    });
-                } catch (Exception e) {
-                    error.setText(e.getMessage());
-                }
-            }
-        });
-        final KeyUpHandler keyHandler = new KeyUpHandler() {
-                    @Override
-                    public void onKeyUp(KeyUpEvent event) {
-                        if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
-                            loginButton.fireEvent(new SelectEvent());
-                        }
-                    }
-        };
-        loginBox.addKeyUpHandler(keyHandler);
-        passwordBox.addKeyUpHandler(keyHandler);
-        verticalPanel.add(loginButton);
+        dialogBox = new LoginBox();
         RootPanel.get().add(dialogBox);
         dialogBox.hide();
     }
---
+++ src/main/java/botleecher/client/component/LoginBox.java
@@ -0,0 +1,88 @@
+package botleecher.client.component;
+
+import botleecher.client.BotLeecherGwt;
+import botleecher.client.LoginService;
+import botleecher.client.LoginServiceAsync;
+import botleecher.client.domain.SessionClient;
+import botleecher.server.security.SessionManager;
+import com.google.gwt.event.dom.client.KeyCodes;
+import com.google.gwt.event.dom.client.KeyUpEvent;
+import com.google.gwt.event.dom.client.KeyUpHandler;
+import com.google.gwt.user.client.Cookies;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.*;
+import com.sencha.gxt.widget.core.client.button.TextButton;
+import com.sencha.gxt.widget.core.client.event.SelectEvent;
+
+import java.util.Date;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Maxime Guennec
+ * Date: 23/08/13
+ * Time: 13:10
+ * To change this template use File | Settings | File Templates.
+ */
+public class LoginBox extends DialogBox {
+    private static LoginServiceAsync loginService = LoginService.App.getInstance();
+
+    public LoginBox() {
+        super();
+        setText("Login Verification");
+        final VerticalPanel verticalPanel = new VerticalPanel();
+        add(verticalPanel);
+        final Label error = new Label();
+        verticalPanel.add(error);
+        final TextBox loginBox = new TextBox();
+        final PasswordTextBox passwordBox = new PasswordTextBox();
+        final Grid grid = new Grid(2, 2);
+        grid.setWidget(0, 0, new Label("Login"));
+        grid.setWidget(1, 0, new Label("Password"));
+        grid.setWidget(0, 1, loginBox);
+        grid.setWidget(1, 1, passwordBox);
+        verticalPanel.add(grid);
+
+        final TextButton loginButton = new TextButton("Login");
+        loginButton.addSelectHandler(new SelectEvent.SelectHandler() {
+            @Override
+            public void onSelect(SelectEvent selectEvent) {
+                try {
+                    final String login = loginBox.getText();
+                    loginService.login(login, passwordBox.getText(), new AsyncCallback<String>() {
+
+                        @Override
+                        public void onFailure(Throwable caught) {
+                            error.setText("Error during login.");
+                        }
+
+                        @Override
+                        public void onSuccess(String result) {
+                            if (result == null) {
+                                error.setText("Wrong Login/Password.");
+                            } else {
+                                BotLeecherGwt.setSession(new SessionClient(login, result));
+                                Date expires = new Date(System.currentTimeMillis() + SessionManager.SESSION_DURATION);
+                                Cookies.setCookie(BotLeecherGwt.SID_COOKIE_NAME, result, expires, null, "/", false);
+                                Cookies.setCookie(BotLeecherGwt.USER_COOKIE_NAME, login, expires, null, "/", false);
+                                BotLeecherGwt.showPanel();
+                            }
+                        }
+                    });
+                } catch (Exception e) {
+                    error.setText(e.getMessage());
+                }
+            }
+        });
+        final KeyUpHandler keyHandler = new KeyUpHandler() {
+            @Override
+            public void onKeyUp(KeyUpEvent event) {
+                if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
+                    loginButton.fireEvent(new SelectEvent());
+                }
+            }
+        };
+        loginBox.addKeyUpHandler(keyHandler);
+        passwordBox.addKeyUpHandler(keyHandler);
+        verticalPanel.add(loginButton);
+    }
+}
